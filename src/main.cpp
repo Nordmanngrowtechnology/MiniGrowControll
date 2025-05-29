774d570a533a3fa0e360546a6c9b12b4931d6336
Roger Gerecke <roger.gerecke@gmail.com>
2025-05-29 23:29:10 +0200

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,62 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+
+#define LED_BUILTIN 2;
+
+// Replace with your network credentials
+const char* ssid     = "REPLACE_WITH_YOUR_SSID";
+const char* password = "REPLACE_WITH_YOUR_PASSWORD";
+
+// Define NTP Client to get time
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP);
+
+void setup() {
+    // Initialize Serial Monitor
+    Serial.begin(115200);
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+
+    // Initialize a NTPClient to get time
+    timeClient.begin();
+    // Set offset time in seconds to adjust for your timezone, for example:
+    // GMT +1 = 3600
+    // GMT +8 = 28800
+    // GMT -1 = -3600
+    // GMT 0 = 0
+    timeClient.setTimeOffset(3600);
+}
+
+void loop() {
+    while(!timeClient.update()) {
+        timeClient.forceUpdate();
+    }
+    // The formattedDate comes with the following format:
+    // 2018-05-28T16:00:13Z
+    // We need to extract date and time
+    const String formattedTime = timeClient.getFormattedTime();
+    Serial.println(formattedTime);
+
+    // Extract date
+    const int splitT = formattedTime.indexOf("T");
+    const String dayStamp = formattedTime.substring(0, splitT);
+    Serial.print("DATE: ");
+    Serial.println(dayStamp);
+    // Extract time
+    const String timeStamp = formattedTime.substring(splitT+1, formattedTime.length()-1);
+    Serial.print("HOUR: ");
+    Serial.println(timeStamp);
+    delay(1000);
+}
